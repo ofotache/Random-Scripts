531c93909b38e2ad770f67149a7d437f55df98e6
Ovidiu Fotache <oviteodor@gmail.com>
2019-02-09 23:19:09 +0200

    Create hostapd_clients.sh

---
+++ hostapd_clients.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+echo
+echo "-------------------------"
+echo "DNS Client Leases"
+echo "-------------------------"
+echo "MAC Adress 	    IP Adress     Name" 
+cat /var/lib/misc/dnsmasq.leases | awk '{print $2"   "$3"      "$4}'
+# dhcp_release - Release a DHCP lease on a the local dnsmasq DHCP server.  <--- NOT AVAILABLE, BUT USEFUl
+
+echo
+echo "-------------------------"
+echo "Count Connected Devices"
+echo "-------------------------"
+# as ROOT
+WCLIENTNO=$(hostapd_cli all_sta | grep -c dot11RSNAStatsSTAAddress);
+echo $WCLIENTNO
+
+echo
+echo "-------------------------"
+echo "Connected Devices MAC's"
+echo "-------------------------"
+hostapd_cli all_sta | grep dot11RSNAStatsSTAAddress | awk '{print substr ($0, 26)}';
+echo $WCLIENTMACS
+
+echo
+echo "-------------------------"
+echo "IPv4 Network Neighbour Cache"
+echo "from Kernel Address Resolution Protocol"
+echo "-------------------------"
+arp -e -n
+
+echo
+echo
